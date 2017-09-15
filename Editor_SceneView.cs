0e4478a2922b4d47a26348a99736cbe5d3b5f3ab
DESKTOP-639K08G\morph <ccg1120@naver.com>
2017-09-15 14:54:34 +0900

    에디터 관련 스크립트 - SceneView에 버튼을 만들 수 있는 에디터 스크립트

---
+++ Editor_SceneView.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+/// <summary>
+/// 씬뷰에 버튼을 생성하여 나타나게 하는 스크립트
+/// </summary>
+
+public class Editor_SceneView : Editor {
+
+    [MenuItem("Window/Scene GUI/Enable")]
+    public static void Enable()
+    {
+        SceneView.onSceneGUIDelegate += OnScene;
+        Debug.Log("Scene GUI : Enabled");
+    }
+
+    [MenuItem("Window/Scene GUI/Disable")]
+    public static void Disable()
+    {
+        SceneView.onSceneGUIDelegate -= OnScene;
+        Debug.Log("Scene GUI : Disabled");
+    }
+
+    private static void OnScene(SceneView sceneview)
+    {
+        Handles.BeginGUI();
+        if (GUILayout.Button("Press Me"))
+            Debug.Log("Got it to work.");
+
+        Handles.EndGUI();
+    }
+}
