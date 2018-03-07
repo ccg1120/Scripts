562d1187822cea7ddfbbb4ac045f5cd5aa40a393
ccg1120 <ccg.1120@daum.net>
2018-03-07 22:16:59 +0900

    유니티 Export 링크

---
+++ Export.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class Export
+{
+    [MenuItem("T real/Export")]
+    static void Export()
+    {
+        string browserCore = "Assets/BrowserCore";
+        string trealScriptFolder = "Assets/TrealScript";
+
+        var assets = new string[] { browserCore, trealScriptFolder };
+
+        var export = EditorUtility.DisplayDialog("Export", "Export?", "Export", "Cancel");
+        if (export)
+        {
+            AssetDatabase.ExportPackage(assets, "Assets/TrealBrowserCore.unitypackage", ExportPackageOptions.Recurse);
+            Debug.Log("Export Complete");
+        }
+    }
+}
