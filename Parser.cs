18eeb23115697644363f5c62ed498285b49ff09c
ccg1120 <ccg.1120@daum.net>
2018-03-20 10:42:45 +0900

    csproj 파싱스크립트

---
+++ Parser.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Xml.Linq;
+using System.IO;
+
+public class Parser : MonoBehaviour {
+    public string data;
+    public List<string> XnameList = new List<string>();
+    private void Start()
+    {
+        XDocument csprojdata = XDocument.Load(Application.dataPath + "/Data/Kids.csproj");
+
+        Debug.Log(csprojdata);
+
+        foreach (XElement element in csprojdata.Root.Elements())
+        {
+            XnameList.Add(element.Name.ToString());
+            Debug.Log(element.Name.ToString());
+
+            if (element.Name.ToString().Contains("ItemGroup"))
+            {
+                Debug.Log(element.Name.ToString() + " : ");
+                foreach (XElement elm in element.Elements())
+                {
+                    Debug.Log(elm.Name.ToString());
+                }
+            }
+        }
+    }
+
+}
