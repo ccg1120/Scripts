28a324de84758ae31c91efa56afc78da4b945da7
ccg1120 <ccg.1120@daum.net>
2018-03-21 10:29:07 +0900

    AssetPostProcessort 에 대한 스크립트 추가, 임포트 시 Compile 파일을 체크 하여 경로 및 파일을 추가 해주는 작업 할듯

--- Parser.cs
+++ Parser.cs
@@ -3,13 +3,41 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Xml.Linq;
 using System.IO;
+using System.Linq;
+using UnityEditor;
 
-public class Parser : MonoBehaviour {
-    public string data;
-    public List<string> XnameList = new List<string>();
-    private void Start()
+
+public class Parser : AssetPostprocessor
+{
+    public static List<string> XnameList = new List<string>();
+    public static List<string> ComplieList = new List<string>();
+
+    static void OnPostprocessAllAssets(
+        string[] importedAssets, string[] deletedAssets,
+        string[] movedAssets, string[] movedFromAssetPaths)
     {
-        XDocument csprojdata = XDocument.Load(Application.dataPath + "/Data/Kids.csproj");
+        GetCSPROJData(Application.dataPath + "/Data/Kids.csproj");
+
+        if(importedAssets.Length != 0)
+        {
+            ImportAssetChecker(importedAssets);
+        }
+
+    }
+
+    private static void ImportAssetChecker(string[] array)
+    {
+        var directofry = array.Where(im => im.EndsWith(".cs"))
+           .GroupBy(ia => Path.GetDirectoryName(ia))
+           .Select(g => g.Key);
+
+
+    }
+
+
+    private static void GetCSPROJData(string path)
+    {
+        XDocument csprojdata = XDocument.Load(path);
 
         Debug.Log(csprojdata);
 
@@ -23,6 +51,8 @@ public class Parser : MonoBehaviour {
                 Debug.Log(element.Name.ToString() + " : ");
                 foreach (XElement elm in element.Elements())
                 {
+                    if (!elm.Name.ToString().Contains("Compile")) break;
+                    ComplieList.Add(elm.Name.ToString());
                     Debug.Log(elm.Name.ToString());
                 }
             }
